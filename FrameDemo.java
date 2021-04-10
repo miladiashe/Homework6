a5ff950c1563e0fd80f4577b2b83992b67c9a136
miladiashe <dkdud0122@naver.com>
2021-04-10 20:03:45 +0900

    과제1

---
+++ FrameDemo.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class FrameDemo {
+    public static void main(String[] args) {
+        Frame f = new Frame("FrameTest: Kim");
+        f.setSize(400, 150);
+        f.setVisible(true);
+    }
+
+    
+}
